6d75d90a14136966bea29ba7148dd7814f9dc1a8
adrian_ortuzar <adrianortuzar@gmail.com>
2015-07-23 22:15:02 +0200

    Initial Commit

---
+++ popupProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  popupProject
+//
+//  Created by Adrian Ortuzar on 23/07/15.
+//  Copyright (c) 2015 Adrian Ortuzar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
